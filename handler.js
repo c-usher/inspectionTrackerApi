d486aa97e913a8d41babff02d21cba6721f19708
c-usher <usher.cody@outlook.com>
2021-05-28 15:19:38 -0600

    get request is working for whole list and selected by id, post request is not working properly but is creating a new item, patch is not working, delete is working as intended when selected by id.

---
+++ handler.js
@@ -0,0 +1,98 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+// const bodyParser = require("body-parser");
+const AWS = require("aws-sdk");
+const db = new AWS.DynamoDB.DocumentClient();
+const { v4: uuidv4 } = require("uuid");
+
+app.use(express.urlencoded());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.unsubscribe(bodyParser.json());
+
+app.post("/inspections", async (req, res) => {
+  const data = req.apiGateway.event.body;
+  const params = {
+    TableName: "inspectionTrackTable",
+    Item: {
+      id: uuidv4(),
+      unitNum: data.unitNum,
+      building: data.building,
+      managed: data.managed,
+      rental: data.rental,
+      inHouse: data.inHouse,
+      robCount: data.robeCount,
+      inspected: data.inspected,
+      notes: data.notes,
+    },
+  };
+
+  try {
+    await db.put(params).promise(); //<promise is need for async functions
+    res.status(201).json({ unit: params.Item });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.get("/inspections", async (req, res) => {
+  const params = {
+    TableName: "inspectionTrackTable",
+  };
+
+  const result = await db.scan(params).promise();
+  res.status(200).json({ units: result });
+});
+
+app.get("/inspections/:id", async (req, res) => {
+  const data = req.apiGateway.event.body;
+  const params = {
+    TableName: "inspectionTrackTable",
+    Item: {
+      id: data.id,
+      unitNum: data.unitNum,
+      building: data.building,
+      managed: data.managed,
+      rental: data.rental,
+      inHouse: data.inHouse,
+      robCount: data.robeCount,
+      inspected: data.inspected,
+      notes: data.notes,
+    },
+  };
+     await db.scan(params).promise();
+    res.status(200).json({ unit: params.Item });
+})
+
+app.patch("/inspections/:id", async (req, res) => {
+  const data = req.apiGateway.event.body;
+  const params = {
+    TableName: "inspectionTrackTable",
+    Item: {
+      id: data.id,
+      unitNum: data.unitNum,
+      building: data.building,
+      managed: data.managed,
+      rental: data.rental,
+      inHouse: data.inHouse,
+      robCount: data.robeCount,
+      inspected: data.inspected,
+      notes: data.notes,
+    },
+  };
+  await db.put(params).promise();
+  res.status(200).json({ unit: params.Item });
+});
+
+app.delete("/inspections/:id", async (req, res) => {
+  const params = {
+    TableName: "inspectionTrackTable",
+    Key: {
+      id: req.params.id
+    },
+  };
+
+  await db.delete(params).promise();
+  res.status(200).json({ success: true });
+});
+module.exports.app = serverless(app);
